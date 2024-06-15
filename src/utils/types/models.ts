c1ab40d7e02f40e204aeb14a27ea93154b76b7bb
VivianObiako <vivianobiako1@gmail.com>
2024-06-15 09:08:32 +0100

    feat: added icons and defined icons type

---
+++ src/utils/types/models.ts
@@ -0,0 +1,9 @@
+export type Nullable<T> = T | null;
+
+export interface CustomIconProps {
+    pathFill?: string;
+    svgFill?: string;
+    width?: string | number;
+    height?: string | number;
+    className?: string;
+}
